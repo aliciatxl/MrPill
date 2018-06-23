4657337b9dfc72f121b080f8b61592d0f0aaf3d4
TSY <suyee.tan@ymail.com>
2018-06-23 16:13:15 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/tansuyee/mrpill/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.tansuyee.mrpill;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    FirebaseAuth mAuth;
+    EditText email, password;
+    ProgressBar progressBar;
+
+    private void userLogIn() {
+        String emailString = email.getText().toString();
+        String passwordString = password.getText().toString();
+
+        if (emailString.isEmpty()) {
+            email.setError("Email is required");
+            email.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailString).matches()) {
+            email.setError("Please enter a valid email");
+            email.requestFocus();
+            return;
+        }
+
+        if (passwordString.isEmpty()) {
+            password.setError("Password is required");
+            password.requestFocus();
+            return;
+        }
+
+        if (passwordString.length() < 6) {
+            password.setError("Minimum length of password should be 6");
+            password.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+
+                } else {
+                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonLogIn).setOnClickListener(this);
+        findViewById(R.id.textViewSignUp).setOnClickListener(this);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.relativeLayout).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return true;
+            }
+        });
+
+        Intent intent = getIntent();
+        if (intent.getStringExtra("email") != null) {
+            String emailString = intent.getStringExtra("email").toString();
+            if (emailString != null) {
+                Log.i("emailString", emailString);
+            }
+            email.setText(emailString);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.textViewSignUp:
+                Intent intent = new Intent(this, SignUpActivity.class);
+                startActivity(intent);
+            case R.id.buttonLogIn:
+                userLogIn();
+                break;
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tansuyee/mrpill/SignUpActivity.java
@@ -0,0 +1,114 @@
+package com.example.tansuyee.mrpill;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText email, password;
+    ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
+    private void registerUser() {
+        String emailString = email.getText().toString();
+        String passwordString = password.getText().toString();
+
+        if (emailString.isEmpty()) {
+            email.setError("Email is required");
+            email.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailString).matches()) {
+            email.setError("Please enter a valid email");
+            email.requestFocus();
+            return;
+        }
+
+        if (passwordString.isEmpty()) {
+            password.setError("Password is required");
+            password.requestFocus();
+            return;
+        }
+
+        if (passwordString.length() < 6) {
+            password.setError("Minimum length of password should be 6");
+            password.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(emailString, passwordString)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(SignUpActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(SignUpActivity.this, "Authentication failed. " + task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.buttonSignUp).setOnClickListener(this);
+        findViewById(R.id.textViewLogIn).setOnClickListener(this);
+        findViewById(R.id.relativeLayout).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonSignUp:
+                registerUser();
+                break;
+            case R.id.textViewLogIn:
+                Intent intent = new Intent(this, MainActivity.class);
+                if (email.getText() != null) {
+                    intent.putExtra("email", email.getText().toString());
+                }
+                startActivity(intent);
+        }
+    }
+}
